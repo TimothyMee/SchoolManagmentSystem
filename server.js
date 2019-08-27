bda2c08805eeeac4036009124e364e151870d182
Timothy_Mee <timothy33.tf@gmail.com>
2019-08-27 08:30:47 +0100

    created new express app Connected to db

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+//database connection
+connectDB();
+
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port on ${PORT}`);
+});
